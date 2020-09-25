a9d8bbb68af718a67bcf14c1bf9c053af366e7d2
Tom Durrant <tomchdurrant@gmail.com>
2020-09-25 22:11:17 +0100

    Gatsby Queries

---
+++ starter-files/gatsby/src/components/PizzaList.js
@@ -0,0 +1,26 @@
+import { Link } from 'gatsby';
+import React from 'react';
+
+function SinglePizza({ pizza }) {
+  return (
+    <div>
+      <Link to={`/pizza/${pizza.slug.current}`}>
+        <h2>
+          <span className="mark">{pizza.name}</span>
+        </h2>
+        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      </Link>
+    </div>
+  );
+}
+
+export default function PizzaList({ pizzas }) {
+  console.log(pizzas);
+  return (
+    <div>
+      {pizzas.map((pizza) => (
+        <SinglePizza pizza={pizza} key={pizza.id} />
+      ))}
+    </div>
+  );
+}
--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -1,9 +1,38 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+  console.log(pizzas);
   return (
     <>
-      <p>PizzasPage</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
